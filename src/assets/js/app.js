35d6b7c1de258d8042a0f97491b645766093c1bf
Alex Negodiaev <alex.negodiaev@gmail.com>
2018-05-18 23:57:27 +0500

    Customized default variables and added styles to the header

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -24,7 +24,9 @@ document.addEventListener('DOMContentLoaded', function(event) {
       el: '#hero-slider-pagination',
       clickable: true
     },
-    breakpoints: {}
+    breakpoints: {
+      1023: {}
+    }
   });
   //------------------------------------------------------------------------//
 
@@ -90,20 +92,5 @@ document.addEventListener('DOMContentLoaded', function(event) {
       }
     }
   });
-  //------------------------------------------------------------------------//
-
-  //------------------------------------------------------------------------//
-  // Map
-  // function initMap() {
-  //   var uluru = { lat: -25.363, lng: 131.044 };
-  //   var map = new google.maps.Map(document.getElementById('map'), {
-  //     zoom: 4,
-  //     center: uluru
-  //   });
-  //   var marker = new google.maps.Marker({
-  //     position: uluru,
-  //     map: map
-  //   });
-  // }
   //------------------------------------------------------------------------//
 });
